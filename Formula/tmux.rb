4c0a45b27e58c3a2b867997196fcc1bf00818c52
miniturbo <kazuya.shono@gmail.com>
2015-05-10 01:26:46 +0900

    upgrade tmux 2.0

--- Formula/tmux.rb
+++ Formula/tmux.rb
@@ -2,8 +2,15 @@ require 'formula'
 
 class Tmux < Formula
   homepage 'http://tmux.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/tmux/tmux/tmux-1.9/tmux-1.9a.tar.gz'
-  sha1 '815264268e63c6c85fe8784e06a840883fcfc6a2'
+  url 'https://downloads.sourceforge.net/project/tmux/tmux/tmux-2.0/tmux-2.0.tar.gz'
+  sha1 '977871e7433fe054928d86477382bd5f6794dc3d'
+
+  bottle do
+    cellar :any
+    sha256 "91a14274005416c9a20f64f149f732837b0503c0ddcfdc80f87c0576e99ee3fa" => :yosemite
+    sha256 "d70b62ddf26d2113a108622643550dc50248c98188af27d7e2e76e415f43588d" => :mavericks
+    sha256 "a1468fd6ac69c18c4773a65c11b2811525d542d911f6c6642e87c0e195f6c4c1" => :mountain_lion
+  end
 
   head do
     url 'git://git.code.sf.net/p/tmux/tmux-code'
